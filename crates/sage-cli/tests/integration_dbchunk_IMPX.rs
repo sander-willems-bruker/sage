b14019241bf1460661acae821f146ea05bbafe5e
ZhaoYaniBruker <147511279+ZhaoYaniBruker@users.noreply.github.com>
2024-09-10 13:01:43 +0200

    Add files via upload
    add prefilter to minimal tests

---
+++ crates/sage-cli/tests/integration_dbchunk_IMPX.rs
@@ -0,0 +1,44 @@
+use sage_core::database::Builder;
+use sage_core::mass::Tolerance;
+use sage_core::scoring::Scorer;
+use sage_core::spectrum::SpectrumProcessor;
+
+#[test]
+fn integration() -> anyhow::Result<()> {
+    let mut builder = Builder::default();
+    builder.prefilter = Some(true);
+    builder.prefilter_chunk_size = Some(1);
+    builder.update_fasta("foo".into());
+
+    let fasta = sage_cloudpath::util::read_fasta("../../tests/Q99536_P06733.fasta", "rev_", true)?;
+    let database = builder.make_parameters().build(fasta);
+    let spectra = sage_cloudpath::util::read_mzml("../../tests/LQSRPAAPPAPGPGQLTLR.mzML", 0, None)?;
+    assert_eq!(spectra.len(), 1);
+
+    let sp = SpectrumProcessor::new(100, true, 0.0);
+    let processed = sp.process(spectra[0].clone());
+    assert!(processed.peaks.len() <= 300);
+
+    let scorer = Scorer {
+        db: &database,
+        precursor_tol: Tolerance::Ppm(-50.0, 50.0),
+        fragment_tol: Tolerance::Ppm(-10.0, 10.0),
+        min_matched_peaks: 4,
+        min_isotope_err: -1,
+        max_isotope_err: 3,
+        min_precursor_charge: 2,
+        max_precursor_charge: 4,
+        override_precursor_charge: false,
+        max_fragment_charge: Some(1),
+        chimera: false,
+        report_psms: 1,
+        wide_window: false,
+        annotate_matches: false,
+    };
+
+    let psm = scorer.score(&processed);
+    assert_eq!(psm.len(), 1);
+    assert_eq!(psm[0].matched_peaks, 21);
+
+    Ok(())
+}
